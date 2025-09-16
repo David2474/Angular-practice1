bac35623050d5da1b9e2dbd98387a29aed44a523
dev.david <dg427938@gmail.com>
2025-09-16 14:20:42 -0600

    add signals for the counter component

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
   imports: [],
   templateUrl: './counter-page.component.html',
 })
-export class CounterPageComponent { }
+export class CounterPageComponent {
+  counter = signal(10);
+
+  increaseBy(value: number = 1){
+    this.counter.update( current => current + 1);
+  }
+
+  decreaseBy(value: number = 1){
+    this.counter.update( current => current -1);
+  }
+
+  constructor() {
+
+    let savedValue = localStorage.getItem("counterValue")
+    if(savedValue){
+      this.counter.set(Number(savedValue))
+    }
+
+    effect(() =>{
+      console.log("changed value", this.counter());
+      localStorage.setItem("counterValue", this.counter().toString());
+    });
+  }
+ }
